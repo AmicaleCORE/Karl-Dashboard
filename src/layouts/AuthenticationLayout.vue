c48186baa61b772a6c11d87b6fd59b7a87e06b1f
loicmaes <production@maesloic.fr>
2023-03-28 23:07:42 +0200

    :rocket: Default layouts (need to be populated)

---
+++ src/layouts/AuthenticationLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <RouterView />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AuthenticationLayout'
+})
+</script>
+
+<style lang="sass" scoped>
+
+</style>
