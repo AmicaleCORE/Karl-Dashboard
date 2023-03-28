ccc95b623f3b31554370888ba0a8324adb9ceaad
loicmaes <production@maesloic.fr>
2023-03-29 01:40:13 +0200

    :rocket: Icons resources & renderer component

---
+++ src/components/framework/misc/IconRenderer.vue
@@ -0,0 +1,36 @@
+<template>
+  <span class="icon" v-if="icon" v-html="icon"></span>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { icons } from '@/assets/scripts/icons'
+
+export default defineComponent({
+  name: 'IconRenderer',
+  data: () => ({
+    icon: undefined
+  }),
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    category: {
+      type: String,
+      required: true
+    }
+  },
+  created () {
+    if (!icons[this.category]) return
+    this.icon = icons[this.category][this.name]
+  }
+})
+</script>
+
+<style lang="sass" scoped>
+.icon
+  height: 1em
+  display: block
+  width: fit-content
+</style>
