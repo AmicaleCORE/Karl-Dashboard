dc069db979e0ee8d6c91c36d4b5a588a249d6d19
loicmaes <production@maesloic.fr>
2023-04-07 16:04:55 +0200

    :rocket: Selling view (wip)

---
+++ src/assets/scripts/products.ts
@@ -0,0 +1,74 @@
+export const products = [
+  {
+    image: 'https://images.unsplash.com/photo-1554866585-cd94860890b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80',
+    name: 'Coca-Cola',
+    price: 0.70,
+    stock: 19
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1630404365865-97ff92feba6a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+    name: 'Coca-Cola Zéro',
+    price: 0.60,
+    stock: 80
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1559181567-c3190ca9959b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+    name: 'Coca-Cola Cherry',
+    price: 0.70,
+    stock: 29
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1546695244-22170c3ade72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80',
+    name: 'Pepsi',
+    price: 0.70,
+    stock: 48
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1642372849486-f88b963cb734?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+    name: 'Ice-Tea',
+    price: 0.60,
+    stock: 30
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1622766815178-641bef2b4630?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1219&q=80',
+    name: '7Up',
+    price: 0.50,
+    stock: 77
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1619241638225-14d56e47ae64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
+    name: 'Orangina',
+    price: 0.70,
+    stock: 100
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1614887065001-06c958a7cddd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+    name: 'Eau',
+    price: 0.40,
+    stock: 71
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1501740326664-5571ff5e30a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80',
+    name: 'Eau pétillante',
+    price: 0.60,
+    stock: 0
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1604815891325-0f9c17688328?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    name: 'KitKat',
+    price: 0.50,
+    stock: 2
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1621939514649-280e2ee25f60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+    name: 'Mars',
+    price: 0.50,
+    stock: 0
+  },
+  {
+    image: 'https://images.unsplash.com/photo-1509042239860-f550ce710b93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+    name: 'Café noisette',
+    price: 0.30,
+    stock: 0
+  }
+]
--- src/views/products/SellingView.vue
+++ src/views/products/SellingView.vue
@@ -1,16 +1,121 @@
 <template>
   <main class="selling">
+    <header class="selling__filters">
+      Filtres
+    </header>
+
+    <section class="selling__products">
+      <div class="selling__products__item" @click="click(id)" :class="{ disabled: product.stock <= 0, preferred: false }" :key="product" v-for="(product, id) in products">
+        <img :src="product.image" alt="" class="selling__products__item--thumbnail" />
+
+        <div class="selling__products__item--content">
+          <h3 class="selling__products__item--name">{{ product.name }}</h3>
+
+          <div class="selling__products__item__creds">
+            <p class="selling__products__item__creds--price">{{ product.price.toFixed(2) }} €</p>
+            <p class="selling__products__item__cred--amount">{{ product.stock }}</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="selling__command">
+      <p :key="product" v-for="product in store.cart">{{ product.name }}</p>
+    </section>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { products } from '@/assets/scripts/products'
 
 export default defineComponent({
-  name: 'SellingView'
+  name: 'SellingView',
+  data: () => ({
+    products,
+    store: {
+      cart: []
+    }
+  }),
+  methods: {
+    click (id: number) {
+      if (!this.products[id] || this.products[id].stock <= 0) return
+      // Add to cart
+      this.store.cart.push(this.products[id] as never)
+    }
+  }
 })
 </script>
 
-<style scoped>
+<style lang="sass" scoped>
+@use "../../assets/css/book"
 
+.selling
+  gap: 2em
+  display: grid
+  grid-template-columns: repeat(3, 1fr)
+
+  &__filters
+    grid-column: span 3
+
+  &__products
+    display: grid
+    gap: 1.5em 1em
+    grid-column: span 2
+    background-color: book.$bg
+    grid-template-columns: repeat(auto-fill, minmax(12.5em, 1fr))
+
+    &__item
+      gap: 1em
+      display: flex
+      cursor: pointer
+      border-radius: 1em
+      padding: 1em 1.5em
+      align-items: center
+      flex-direction: column
+      box-shadow: 0 .25em 1em 0 book.$shadow-tint
+
+      &.preferred
+        order: -1
+
+      &.disabled
+        order: 1
+        cursor: not-allowed
+
+        > *
+          opacity: .3
+          pointer-events: none
+
+      *
+        user-select: none
+
+      &--content
+        gap: .5em
+        width: 100%
+        height: 100%
+        display: flex
+        flex-direction: column
+        justify-content: space-between
+
+      &--thumbnail
+        width: 3em
+        object-fit: cover
+        border-radius: 1em
+        aspect-ratio: 1 / 1
+        transition: transform 250ms ease-in-out
+
+      &--name
+        font-weight: 600
+        font-size: 1.25em
+        line-height: 100%
+
+      &__creds
+        width: 100%
+        display: flex
+        align-items: center
+        justify-content: space-between
+
+      &:not(.disabled):hover
+        > img
+          transform: scale(1.07) translateY(-.35em)
 </style>
