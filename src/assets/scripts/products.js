13f359eb79bdaa3418c3592ca8554c2d81f7ef69
loicmaes <production@maesloic.fr>
2023-04-08 15:32:31 +0200

    :rocket: Product management mock scripts

--- src/assets/scripts/products.js
+++ src/assets/scripts/products.js
@@ -1,74 +1,74 @@
-export default [
-  {
+export default {
+  coca_cola: {
     image: 'https://images.unsplash.com/photo-1554866585-cd94860890b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80',
     name: 'Coca-Cola',
     price: 0.70,
     stock: 19
   },
-  {
+  coca_cola_zero: {
     image: 'https://images.unsplash.com/photo-1630404365865-97ff92feba6a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
     name: 'Coca-Cola Zéro',
     price: 0.60,
     stock: 80
   },
-  {
+  coca_cola_cherry: {
     image: 'https://images.unsplash.com/photo-1559181567-c3190ca9959b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
     name: 'Coca-Cola Cherry',
     price: 0.70,
     stock: 29
   },
-  {
+  pepsi: {
     image: 'https://images.unsplash.com/photo-1546695244-22170c3ade72?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1331&q=80',
     name: 'Pepsi',
     price: 0.70,
     stock: 48
   },
-  {
+  ice_tea: {
     image: 'https://images.unsplash.com/photo-1642372849486-f88b963cb734?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
     name: 'Ice-Tea',
     price: 0.60,
     stock: 30
   },
-  {
+  seven_up: {
     image: 'https://images.unsplash.com/photo-1622766815178-641bef2b4630?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1219&q=80',
     name: '7Up',
     price: 0.50,
     stock: 77
   },
-  {
+  orangina: {
     image: 'https://images.unsplash.com/photo-1619241638225-14d56e47ae64?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80',
     name: 'Orangina',
     price: 0.70,
     stock: 100
   },
-  {
+  eau: {
     image: 'https://images.unsplash.com/photo-1614887065001-06c958a7cddd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
     name: 'Eau',
     price: 0.40,
     stock: 71
   },
-  {
+  eau_petillante: {
     image: 'https://images.unsplash.com/photo-1501740326664-5571ff5e30a6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80',
     name: 'Eau pétillante',
     price: 0.60,
     stock: 0
   },
-  {
+  kitkat: {
     image: 'https://images.unsplash.com/photo-1604815891325-0f9c17688328?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
     name: 'KitKat',
     price: 0.50,
     stock: 2
   },
-  {
+  mars: {
     image: 'https://images.unsplash.com/photo-1621939514649-280e2ee25f60?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
     name: 'Mars',
     price: 0.50,
     stock: 0
   },
-  {
+  cafe_noisette: {
     image: 'https://images.unsplash.com/photo-1509042239860-f550ce710b93?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
     name: 'Café noisette',
     price: 0.30,
     stock: 0
   }
-]
+}
