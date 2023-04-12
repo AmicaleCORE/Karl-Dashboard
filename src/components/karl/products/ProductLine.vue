1d2021948fbc395def38c585411d7b17d7a0ae8a
loicmaes <production@maesloic.fr>
2023-04-12 21:21:31 +0200

    :rocket: New selling panel version

--- src/components/karl/products/ProductLine.vue
+++ src/components/karl/products/ProductLine.vue
@@ -1,20 +1,22 @@
 <template>
-  <div v-if="product" @click.prevent="$emit('clicked', { product })" class="product" :class="{ preferred: false, disabled: product.stock <= 0 }">
+  <div v-if="product" @click.prevent="$emit('clicked', { product })" class="product" :class="{ preferred: false, disabled: product.stock <= 0, unavailable: $store.state.cart[product.name]?.amount >= product.stock }">
     <img :src="product.image" alt="" class="product--thumbnail" />
     <p class="product--name">{{ product.name }}</p>
 
     <article class="product__info">
-      <p class="product__info--price">{{ product.price.toFixed(2).replace('.', ',').replace(/\B(?=(\d{3})+(?!\d))/g, ' ') }} €</p>
-      <p class="product__info--quantity">{{ product.stock }}</p>
+      <p class="product__info--price">{{ parseMoney(product.price) }} €</p>
+      <p class="product__info--quantity" :class="{ tooltip: product.stock >= 1000 }" :data-amount="product.stock">{{ product.stock >= 1000 ? '999+' : product.stock }}</p>
     </article>
   </div>
 </template>
 
 <script>
 import { defineComponent } from 'vue'
+import { parseMoney } from '@/assets/scripts/functions'
 
 export default defineComponent({
   name: 'ProductLine',
+  methods: { parseMoney },
   props: {
     product: {
       type: Object,
@@ -39,13 +41,15 @@ export default defineComponent({
   &.preferred
     order: -1
 
-  &.disabled
-    order: 1
+  &.disabled, &.unavailable
     cursor: not-allowed
 
     > *
       opacity: .3
       pointer-events: none
+
+  &.disabled
+    order: 1
 
   *
     user-select: none
@@ -62,7 +66,6 @@ export default defineComponent({
 
   &__info
     gap: 1em
-    height: 75%
     display: flex
     flex: 0 0 auto
     align-items: center
@@ -70,11 +73,39 @@ export default defineComponent({
     &::before
       width: 1px
       content: ''
+      height: 2em
       opacity: .15
-      height: 100%
       display: block
       background-color: currentColor
 
     &--price
       order: -1
+
+    &--quantity
+      width: 3ch
+      line-height: 2em
+      text-align: right
+      position: relative
+
+      &.tooltip::before
+        right: 0
+        opacity: 0
+        bottom: 100%
+        color: book.$bg
+        line-height: 100%
+        user-select: none
+        padding: .5em 1em
+        position: absolute
+        border-radius: .5em
+        pointer-events: none
+        background: book.$txt
+        content: attr(data-amount)
+        transition-duration: 350ms
+        transform: translateY(-.5em)
+        transition-timing-function: ease-in-out
+        transition-property: opacity, transform
+
+      &.tooltip:hover::before
+        opacity: 1
+        transform: none
 </style>
---
+++ src/components/karl/products/cart/CartProduct.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="product">
+    <section class="product__infos">
+      <p class="product__infos--name">{{ product.name }}</p>
+      <div class="product__infos__actions">
+        <button class="product__infos__actions--button" :aria-label="`Retirer un ${product.name}`" @click.prevent="submit('decrease', { target: product, amount: 1 })"><IconRenderer class="product__infos__actions--button-icon" category="default" name="minus" /></button>
+        <p class="product__infos__actions--amount">{{ product.amount }}</p>
+        <button class="product__infos__actions--button" :class="{ disabled: unavailable }" :aria-label="`Ajouter un ${product.name}`" @click.prevent="submit('increase', { target: product, amount: 1 }, unavailable)"><IconRenderer class="product__infos__actions--button-icon" category="default" name="plus" /></button>
+      </div>
+    </section>
+
+    <aside class="product__resume">
+      <p class="product__resume--price">{{ parseMoney(getTotal()) }} €</p>
+      <button class="product__resume--clear" @click.prevent="submit('clear', { target: product })"><IconRenderer category="outline" name="trash" /></button>
+    </aside>
+  </div>
+</template>
+
+<script>
+import { parseMoney } from '@/assets/scripts/functions'
+import IconRenderer from '@/components/framework/misc/IconRenderer.vue'
+
+export default {
+  name: 'CartProduct',
+  components: { IconRenderer },
+  data: () => ({
+    unavailable: true
+  }),
+  created () {
+    this.unavailable = this.product.amount >= this.product.stock
+  },
+  updated () {
+    this.unavailable = this.product.amount >= this.product.stock
+  },
+  props: {
+    product: {
+      type: Object
+    }
+  },
+  methods: {
+    parseMoney,
+    getTotal () {
+      return this.product.amount * this.product.price
+    },
+    submit (event, options, condition) {
+      if (!condition) this.$emit(event, options)
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@use '../../../../assets/css/book'
+
+.product
+  gap: .5em
+  display: flex
+  padding: .5em 0
+  justify-content: space-between
+
+  *
+    user-select: none
+
+  &__infos
+    gap: .5em
+    display: flex
+    flex-direction: column
+
+    &--name
+      font-weight: 600
+
+    &__actions
+      gap: .5em
+      display: flex
+      align-items: center
+
+      &--button
+        width: 1.5em
+        display: grid
+        border-radius: .5em
+        aspect-ratio: 1 / 1
+        place-content: center
+        background: book.$bg-accent
+
+        &.disabled
+          cursor: not-allowed
+
+      &--amount
+        opacity: .6
+
+  &__resume
+    gap: .5em
+    display: flex
+    align-items: center
+
+    &--price
+      white-space: nowrap
+
+    &--clear
+      height: 100%
+      padding: 0 .5em
+      color: book.$alert
+</style>
